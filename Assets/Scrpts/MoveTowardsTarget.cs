d5e923a1c623de8db152c501cdf3e28beadaee6d
BradSlavens <bradslavens@gmail.com>
2023-04-29 22:34:05 -0700

    working but buggy

--- Assets/Scrpts/MoveTowardsTarget.cs
+++ Assets/Scrpts/MoveTowardsTarget.cs
@@ -2,44 +2,119 @@ using UnityEngine;
 
 public class MoveTowardsTarget : MonoBehaviour
 {
-    public GameObject target; // The target game object the NPC will move towards
-    public float speed = 5f; // The speed at which the NPC will move towards the target
-    public float stoppingDistance = 1f; // The distance at which the NPC will stop moving towards the target
-    public float avoidDistance = 2f; // The distance at which the NPC will start avoiding obstacles
-    public LayerMask obstacleLayer; // The layer(s) that represent obstacles
+    public Transform target;
+    public float forwardRayDistance = 5f;
+    public float downRayDistance = 5f;
 
-    private bool isMoving = true;
+    public float walkingSpeed = 5f;
+    public float climbingSpeed = 2f;
+    public float fallingSpeed = 10f;
+
+    public enum NPCState
+    {
+        Climbing,
+        Walking,
+        Falling
+    }
+
+    private NPCState npcState;
 
     private void Update()
     {
-        // If the target is null or the NPC has already reached the target, exit the method
-        if (target == null || !isMoving)
-            return;
+        TurnNPCTowardsTarget(target);
+        MoveNPCTowardsTarget(target);
+        SetNPCState(forwardRayDistance, downRayDistance);
+    }
 
-        // Calculate the direction from the NPC to the target
-        Vector3 direction = (target.transform.position - transform.position).normalized;
-
-        // Cast a ray in the direction of the target to check for obstacles
+    private bool ShootRayForward(float rayDistance)
+    {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, direction, out hit, avoidDistance, obstacleLayer))
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, rayDistance))
         {
-            // If an obstacle is detected, steer the NPC away from the obstacle
-            Vector3 avoidDirection = Vector3.Reflect(direction, hit.normal);
-            transform.rotation = Quaternion.LookRotation(avoidDirection);
+            // Hit something
+            Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.green);
+            return true;
         }
         else
         {
-            // If no obstacle is detected, move towards the target
-            transform.rotation = Quaternion.LookRotation(direction);
-            if (Vector3.Distance(transform.position, target.transform.position) > stoppingDistance)
-            {
-                transform.position += transform.forward * speed * Time.deltaTime;
-            }
-            else
-            {
-                isMoving = false;
-                Debug.Log("Reached target!");
-            }
+            // Did not hit anything
+            Debug.DrawRay(transform.position, transform.forward * rayDistance, Color.red);
+            return false;
         }
     }
+
+    private bool ShootRayDown(float rayDistance)
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, rayDistance))
+        {
+            // Hit something
+            Debug.DrawRay(transform.position, -Vector3.up * hit.distance, Color.green);
+            return true;
+        }
+        else
+        {
+            // Did not hit anything
+            Debug.DrawRay(transform.position, -Vector3.up * rayDistance, Color.red);
+            return false;
+        }
+    }
+
+    public void SetNPCState(float forwardRayDistance, float downRayDistance)
+    {
+        if (ShootRayForward(forwardRayDistance))
+        {
+            // Ray hit something in front of the NPC, so it's climbing
+            npcState = NPCState.Climbing;
+        }
+        else if (ShootRayDown(downRayDistance))
+        {
+            // Ray hit something beneath the NPC, so it's walking
+            npcState = NPCState.Walking;
+        }
+        else
+        {
+            // Ray did not hit anything, so it's falling
+           // npcState = NPCState.Falling;
+        }
+    }
+
+    private void MoveNPCTowardsTarget(Transform target)
+    {
+        if (npcState == NPCState.Walking)
+        {
+            // Move the NPC towards the target
+            if (target != null)
+            {
+                Vector3 direction = (target.position - transform.position).normalized;
+                transform.position += direction * walkingSpeed * Time.deltaTime;
+            }
+        }
+        else if (npcState == NPCState.Climbing)
+        {
+            // Move the NPC up the y-axis
+            transform.position += Vector3.up * climbingSpeed * Time.deltaTime;
+        }
+        else if (npcState == NPCState.Falling)
+        {
+            // Move the NPC down the y-axis
+            transform.position += Vector3.down * fallingSpeed * Time.deltaTime;
+        }
+    }
+
+    public void TurnNPCTowardsTarget(Transform target)
+    {
+        if (target != null)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        }
+    }
+
+
+
+
 }
