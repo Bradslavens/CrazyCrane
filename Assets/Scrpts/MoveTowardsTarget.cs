94f458d5362c4c410ef907d9bd40bc407841bf25
BradSlavens <bradslavens@gmail.com>
2023-04-30 07:48:34 -0700

    npc follow target is getting better but still buggy

--- Assets/Scrpts/MoveTowardsTarget.cs
+++ Assets/Scrpts/MoveTowardsTarget.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
 
+
 public class MoveTowardsTarget : MonoBehaviour
 {
     public Transform target;
-    public float forwardRayDistance = 5f;
-    public float downRayDistance = 5f;
-
+    public float forwardRayDistance = 1f;
+    public float downRayDistance = 1f;
     public float walkingSpeed = 5f;
     public float climbingSpeed = 2f;
     public float fallingSpeed = 10f;
+
+    private NPCState npcState = NPCState.Walking;
 
     public enum NPCState
     {
@@ -17,104 +19,108 @@ public class MoveTowardsTarget : MonoBehaviour
         Falling
     }
 
-    private NPCState npcState;
-
-    private void Update()
+    private void FixedUpdate()
     {
-        TurnNPCTowardsTarget(target);
-        MoveNPCTowardsTarget(target);
+        // Update the state of the NPC
         SetNPCState(forwardRayDistance, downRayDistance);
+
+        // Move the NPC based on its state
+        MoveNPCTowardsTarget();
+
+        // Turn the NPC towards the target
+        TurnNPCTowardsTarget();
     }
 
-    private bool ShootRayForward(float rayDistance)
+    private bool ShootRaycastForward(float rayDistance)
     {
         RaycastHit hit;
 
         if (Physics.Raycast(transform.position, transform.forward, out hit, rayDistance))
         {
-            // Hit something
             Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.green);
             return true;
         }
         else
         {
-            // Did not hit anything
             Debug.DrawRay(transform.position, transform.forward * rayDistance, Color.red);
             return false;
         }
     }
 
-    private bool ShootRayDown(float rayDistance)
+    private bool ShootRaycastDown(float rayDistance)
     {
         RaycastHit hit;
 
         if (Physics.Raycast(transform.position, -Vector3.up, out hit, rayDistance))
         {
-            // Hit something
             Debug.DrawRay(transform.position, -Vector3.up * hit.distance, Color.green);
             return true;
         }
         else
         {
-            // Did not hit anything
             Debug.DrawRay(transform.position, -Vector3.up * rayDistance, Color.red);
             return false;
         }
     }
 
-    public void SetNPCState(float forwardRayDistance, float downRayDistance)
-    {
-        if (ShootRayForward(forwardRayDistance))
-        {
-            // Ray hit something in front of the NPC, so it's climbing
-            npcState = NPCState.Climbing;
-        }
-        else if (ShootRayDown(downRayDistance))
-        {
-            // Ray hit something beneath the NPC, so it's walking
-            npcState = NPCState.Walking;
-        }
-        else
-        {
-            // Ray did not hit anything, so it's falling
-           // npcState = NPCState.Falling;
-        }
-    }
-
-    private void MoveNPCTowardsTarget(Transform target)
+    private void SetNPCState(float forwardRayDistance, float downRayDistance)
     {
         if (npcState == NPCState.Walking)
         {
-            // Move the NPC towards the target
-            if (target != null)
+            if (!ShootRaycastDown(downRayDistance))
             {
-                Vector3 direction = (target.position - transform.position).normalized;
-                transform.position += direction * walkingSpeed * Time.deltaTime;
+                npcState = NPCState.Falling;
+            }
+            else if (ShootRaycastForward(forwardRayDistance))
+            {
+                npcState = NPCState.Climbing;
+            }
+        }
+        else if (npcState == NPCState.Falling)
+        {
+            if (ShootRaycastDown(downRayDistance))
+            {
+                npcState = NPCState.Walking;
             }
         }
         else if (npcState == NPCState.Climbing)
         {
-            // Move the NPC up the y-axis
-            transform.position += Vector3.up * climbingSpeed * Time.deltaTime;
+            if (!ShootRaycastForward(forwardRayDistance))
+            {
+                npcState = NPCState.Walking;
+            }
+        }
+
+    }
+
+    private void MoveNPCTowardsTarget()
+    {
+        if (npcState == NPCState.Walking)
+        {
+            if (target != null)
+            {
+                Vector3 direction = (target.position - transform.position).normalized;
+                transform.position += direction * walkingSpeed * Time.fixedDeltaTime;
+            }
+        }
+        else if (npcState == NPCState.Climbing)
+        {
+            transform.position += Vector3.up * climbingSpeed * Time.fixedDeltaTime;
         }
         else if (npcState == NPCState.Falling)
         {
-            // Move the NPC down the y-axis
-            transform.position += Vector3.down * fallingSpeed * Time.deltaTime;
+            transform.position += Vector3.down * fallingSpeed * Time.fixedDeltaTime;
         }
     }
 
-    public void TurnNPCTowardsTarget(Transform target)
+    private void TurnNPCTowardsTarget()
     {
         if (target != null)
         {
             Vector3 direction = (target.position - transform.position).normalized;
+            direction.y = 0f; // set the y component to zero to lock the rotation to the y-axis
             Quaternion lookRotation = Quaternion.LookRotation(direction);
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
         }
     }
-
-
-
-
 }
