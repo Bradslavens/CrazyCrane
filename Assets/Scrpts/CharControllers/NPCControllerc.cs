2572745322e2ac1fa4c997798dd22cc35ec91b29
BradSlavens <bradslavens@gmail.com>
2023-05-03 18:57:33 -0700

    npc climbs half way up the shoots away

--- Assets/Scrpts/CharControllers/NPCControllerc.cs
+++ Assets/Scrpts/CharControllers/NPCControllerc.cs
@@ -11,17 +11,28 @@ public class NPCControllerc : MonoBehaviour
 
     private Rigidbody rb; // the Rigidbody component
     private bool isGrounded; // whether the NPC is grounded or not
+    private bool isClimbing; // whether the NPC is currently climbing or not
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         isGrounded = false;
+        isClimbing = false;
     }
 
     void FixedUpdate()
     {
+        if (isClimbing)
+        {
+            // if climbing, don't move forward
+            return;
+        }
+
         // check if the NPC is on the ground
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);
+        Vector3 raycastOrigin = transform.position - new Vector3(0.0f, 0.5f, 0.0f);
+        float raycastDistance = 0.5f + 0.1f;
+        RaycastHit hitInfo;
+        isGrounded = Physics.Raycast(raycastOrigin, Vector3.down, out hitInfo, raycastDistance);
 
         // if not, apply gravity
         if (!isGrounded)
@@ -40,28 +51,30 @@ public class NPCControllerc : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision collision)
     {
         // if the NPC collides with an obstacle, climb it
-        if (other.CompareTag("Obstacle"))
+        if (collision.gameObject.CompareTag("Obstacle"))
         {
-            StartCoroutine(ClimbObstacle(other));
+            StartCoroutine(ClimbObstacle(collision));
         }
     }
 
-    IEnumerator ClimbObstacle(Collider obstacle)
+    IEnumerator ClimbObstacle(Collision obstacle)
     {
-        // disable gravity
+        // disable gravity and set climbing flag
         rb.useGravity = false;
+        isClimbing = true;
 
-        // calculate the climb direction
-        Vector3 climbDir = obstacle.transform.up;
+        // calculate the climb direction and distance
+        Vector3 climbDir = obstacle.contacts[0].normal;
         Vector3 climbStart = transform.position;
+        float climbDistance = obstacle.collider.bounds.size.y - 1.0f; // subtract 1.0f to account for NPC height
 
         // move towards the obstacle
-        while (Vector3.Distance(transform.position, obstacle.transform.position) > 0.5f)
+        while (Vector3.Distance(transform.position, obstacle.contacts[0].point) > 0.5f)
         {
-            Vector3 targetDir = (obstacle.transform.position - transform.position).normalized;
+            Vector3 targetDir = (obstacle.contacts[0].point - transform.position).normalized;
             Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, rotationSpeed * Time.deltaTime, 0.0f);
             transform.rotation = Quaternion.LookRotation(newDir);
             transform.position += transform.forward * moveSpeed * Time.deltaTime;
@@ -69,7 +82,7 @@ public class NPCControllerc : MonoBehaviour
         }
 
         // climb up the obstacle
-        while (Vector3.Distance(transform.position, climbStart + climbDir) > 0.5f)
+        while (Vector3.Distance(transform.position, climbStart + climbDir * climbDistance) > 0.5f)
         {
             transform.position += climbDir * climbSpeed * Time.deltaTime;
             yield return null;
@@ -83,4 +96,5 @@ public class NPCControllerc : MonoBehaviour
         // re-enable gravity
         rb.useGravity = true;
     }
+
 }
