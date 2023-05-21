06a0efe31e4d1396e84cedf8dc8375afcc650803
BradSlavens <bradslavens@gmail.com>
2023-05-21 09:37:53 -0700

    added soldier animation

---
+++ Assets/Scrpts/CharacterMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public Transform target;
+    public float rotationSpeed = 5f;
+    public float movementSpeed = 5f;
+    private Animator animator;
+    private bool isMoving = true;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            Vector3 direction = target.position - transform.position;
+            direction.y = 0f; // Ignore any height difference
+            direction.Normalize();
+
+            Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+
+            animator.SetBool("isRunning", true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == target)
+        {
+            isMoving = false;
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
