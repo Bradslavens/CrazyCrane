31bf4f221ca9050ffbff8c0c01a3aa57e370e091
BradSlavens <bradslavens@gmail.com>
2023-06-11 08:33:00 -0700

    fixed friend controller rotation

--- Assets/Scripts/Friends/FriendController.cs
+++ Assets/Scripts/Friends/FriendController.cs
@@ -4,15 +4,23 @@ public class FriendController : MonoBehaviour
 {
     public GameObject target;
     public float speed = 3.0f;
+    public float rotationSpeed = 1.0f; // rotation speed
     public float shootingRange = 1f;
 
     private Animator animator;
+
+    public enum CharacterState
+    {
+        Running,
+        Shooting
+    }
+
+    public CharacterState currentState = CharacterState.Running;
 
     private void Start()
     {
         // Get the Animator component attached to the NPC
         animator = GetComponent<Animator>();
-
     }
 
     void Update()
@@ -25,27 +33,31 @@ public class FriendController : MonoBehaviour
             // Make sure the character moves along the ground plane by setting y to zero
             direction.y = 0;
 
-            // Look at the target (y-axis only)
-            var targetRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.Euler(0, targetRotation.eulerAngles.y, 0);
-
             // Check the distance to the target
             float distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
             if (distanceToTarget <= shootingRange)
             {
-
+                currentState = CharacterState.Shooting;
+            }
+            else
+            {
+                currentState = CharacterState.Running;
             }
 
-
-            if (true)
+            if (currentState == CharacterState.Shooting)
             {
-                // Play the Shooting animation
-                animator.SetTrigger("Shooting");
+                // Set the Shooting boolean to true
+                animator.SetBool("Shooting", true);
             }
-            else if (true)
+            else if (currentState == CharacterState.Running)
             {
+                // Set the Shooting boolean to false
+                animator.SetBool("Shooting", false);
                 // Move the character towards the target
                 transform.position += direction * speed * Time.deltaTime;
+                // Make the character face the target
+                Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
+                transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
             }
         }
     }
