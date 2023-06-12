950ef71885e090cf4c83efd7d1e3fc89ecc4749f
BradSlavens <bradslavens@gmail.com>
2023-06-11 20:23:48 -0700

    fixed error with jenemy list in enemy manager

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,13 +4,18 @@ public class Health : MonoBehaviour, IDamageable
 {
     public int health = 100;
 
+
     public void DealDamage(int amount)
     {
         health -= amount;
         if (health <= 0)
         {
+            EnemyManager enemyManager = GameObject.FindObjectOfType<EnemyManager>();
+            enemyManager.RemoveEnemy(gameObject);
             // Handle death here.
             Destroy(gameObject);
+
+
         }
     }
 }
